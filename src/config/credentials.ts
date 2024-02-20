5f246e57c075663f2a15d64614fa4c2653a93547
BondingTechs <139861052+BondingTechs@users.noreply.github.com>
2024-02-20 17:13:52 +0800

    upload twilo token

--- src/config/credentials.ts
+++ src/config/credentials.ts
@@ -1,11 +1,11 @@
 export const twilio = {
   // serviceSid: 'VA37ac49d0caa3e4c20f1e0ab02b46476a',
   accountSid: 'ACf5f58da6bc9b4cc4892d13fdcf82b3e9',
-  authToken: '4693b13358e5bb887ac760b4e71ff8bb',
+  authToken: '4a0f876cf85ac0e218d9135259b2046f',
   verifySid: 'VAf7f0f6acda0fb5244de1f2876adaba59',
 };
 
 export const googleMail = {
-  user: 'zz02846584zz@gmail.com',
-  pass: 'fakthyzzqejhpvpg',
+  user: "bonding.ume@gmail.com",
+  pass: "dwlqdwslqwtftxvl",
 };
