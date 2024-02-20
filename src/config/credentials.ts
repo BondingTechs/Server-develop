fd30b0fa882110660b32db5db4925e66bdca99e0
BondingTechs <bondingtechs@gmail.com>
2024-02-20 20:48:42 +0800

    update twilo token

--- src/config/credentials.ts
+++ src/config/credentials.ts
@@ -2,10 +2,10 @@ export const twilio = {
   // serviceSid: 'VA37ac49d0caa3e4c20f1e0ab02b46476a',
   accountSid: 'ACf5f58da6bc9b4cc4892d13fdcf82b3e9',
   authToken: '4a0f876cf85ac0e218d9135259b2046f',
-  verifySid: 'VAf7f0f6acda0fb5244de1f2876adaba59',
+  verifySid: 'VAf7f0f6acda0fb5244de1f2876adaba5',
 };
 
 export const googleMail = {
-  user: "bonding.ume@gmail.com",
-  pass: "dwlqdwslqwtftxvl",
+  user: 'bonding.ume@gmail.com',
+  pass: 'dwlqdwslqwtftxvl',
 };
